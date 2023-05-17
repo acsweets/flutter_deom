ce93eb09d904d560fea092358acade637b4932e9
liuxiaocao <liuxiaocao@wehanyu.com>
2023-05-17 14:44:37 +0800

    fix

--- lib/design_pattern/decorator_pattern/decorator.dart
+++ lib/design_pattern/decorator_pattern/decorator.dart
@@ -1,13 +1,5 @@
 ///装饰模式
 ///动态地给一个对象添加一些额外的职责。 就增加功能来说，装饰模式相比生成子类更为灵活。
-///有四个角色需要说明：
-///● Component抽象构件 Component是一个接口或者是抽象类，就是定义我们最核心的对象，也就是最原始的对象，
-///如上面的成绩单。 注意在装饰模式中，必然有一个最基本、最核心、最原始的接口或抽象类充当 Component抽象构件。
-/// ● ConcreteComponent 具体构件 ConcreteComponent是最核心、最原始、最基本的接口或抽象类的实现，你要装饰的就是它。
-/// ● Decorator装饰角色 一般是一个抽象类，做什么用呢？实现接口或者抽象方法，它里面可不一定有抽象的方法呀，
-/// 在它的属性里必然有一个private变量指向Component抽象构件。
-/// ● 具体装饰角色 ConcreteDecoratorA和ConcreteDecoratorB是两个具体的装饰类，
-///你要把你最核心的、最原始的、最基本的东西装饰成其他东西。
 ///
 /// 装饰模式的优点
 /// ● 装饰类和被装饰类可以独立发展，而不会相互耦合。换句话说，Component类无须知 道Decorator类，
@@ -103,7 +95,15 @@ class FouthGradeSchoolReport extends SchoolReport {
 }
 
 ///通用模板===========================================
-///
+///有四个角色需要说明：
+///● Component抽象构件 Component是一个接口或者是抽象类，就是定义我们最核心的对象，也就是最原始的对象，如上面的成绩单。
+///注意在装饰模式中，必然有一个最基本、最核心、最原始的接口或抽象类充当  [Component]抽象构件。
+/// ● [ConcreteComponent ]具体构件 [ConcreteComponent ]是最核心、最原始、最基本的接口或抽象类的实现，你要装饰的就是它。
+/// ● Decorator装饰角色 一般是一个抽象类，做什么用呢？实现接口或者抽象方法，它里面可不一定有抽象的方法呀，
+/// 在它的属性里必然有一个private变量指向Component抽象构件。
+/// ● 具体装饰角色 [ConcreteDecorator1]和[ConcreteDecorator2]是两个具体的装饰类，
+///你要把你最核心的、最原始的、最基本的东西装饰成其他东西。
+///丑陋的成绩单装饰成老爹认可的
 
 abstract class Component {
   void operate();
